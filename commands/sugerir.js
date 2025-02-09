644a2747d3854e3ab80e7cb6f27e331cc987a851
gatita00miau <35899397-gatita00miau@users.noreply.replit.com>
2025-02-09 19:49:14 +0000

    Assistant checkpoint: Added normal suggestion command
    Assistant generated file changes:
    - commands/sugerir.js: Add normal suggestion command
    
    ---
    
    User prompt:
    
    como hago el comando de @slashCommands/sugerir.js para que sea comando normal en la carpeta de commands

---
+++ commands/sugerir.js
@@ -0,0 +1,32 @@
+
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'sugerir',
+  description: 'Envía una sugerencia para el servidor',
+  execute: async (message, args) => {
+    // Verificar si hay una sugerencia
+    if (!args.length) {
+      return message.reply('¡Debes escribir una sugerencia!');
+    }
+
+    const sugerencia = args.join(' ');
+    
+    const embed = new EmbedBuilder()
+      .setTitle('📝 Nueva Sugerencia')
+      .setDescription(sugerencia)
+      .setColor(0x3498DB)
+      .setAuthor({
+        name: message.author.tag,
+        iconURL: message.author.displayAvatarURL()
+      })
+      .setTimestamp()
+      .setFooter({ text: '💡 Reacciona para votar' });
+
+    const mensaje = await message.channel.send({ embeds: [embed] });
+
+    // Agregar reacciones para votar
+    await mensaje.react('👍');
+    await mensaje.react('👎');
+  },
+};
