bd554aa008fb213b5d7afc43603301163cc94aab
Gatita90759 <gatita00miau@gmail.com>
2025-02-23 02:48:28 +0000

    Eliminar cmd d milda (insignia)

---
+++ deleteCommand.js
@@ -0,0 +1,24 @@
+const { REST, Routes } = require('discord.js');
+module.exports = {
+  TOKEN: process.env.TOKEN,
+  CLIENT_ID: process.env.CLIENT_ID,
+    
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log('Eliminando comando específico...');
+        const commands = await rest.get(Routes.applicationCommands(clientId));
+
+        const commandToDelete = commands.find(cmd => cmd.name === 'active-dev-badge');
+
+        if (commandToDelete) {
+            await rest.delete(Routes.applicationCommand(clientId, commandToDelete.id));
+            console.log(`Comando /active-dev-badge eliminado.`);
+        } else {
+            console.log('No se encontró el comando.');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+})();
