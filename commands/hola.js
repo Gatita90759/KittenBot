15beb0b7dd11715a9071f3f3aa7534e19cd1fb0f
Gatita90759 <gatita00miau@gmail.com>
2025-07-06 00:03:49 +0000

    Simplificar

--- commands/hola.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'hola',
-  description: 'Te saluda (prueba)',
-  execute: (message) => {
-    message.reply('holaa! como estas? <:ns:1340712244706676738> ');
-  },
-};
--- models/economy.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const economySchema = new mongoose.Schema({
-  userID: { type: String, required: true, unique: true },
-  balance: { type: Number, default: 0 },
-  lastWorkedAt: { type: Date, default: null } // Nuevo campo para la fecha del último trabajo
-});
-
-module.exports = mongoose.model("Economy", economySchema);
